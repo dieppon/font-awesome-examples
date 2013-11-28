9278559cbe35db45e2424dfc301f4bc2219cc324
Victor Dieppa Garriga <victor-dg@lightflows.co.uk>
2013-11-28 18:20:23 +0000

    First commit with HTML, SCSS and JS

---
+++ js/scripts.js
@@ -0,0 +1,31 @@
+jQuery(document).ready(function($) {
+
+	$("button.colour").click(function(){
+		$("ul.icons").toggleClass("colour");
+		$(this).toggleClass("active");
+		$(".language-css.colour").toggleClass("hide");
+	});
+
+	$("button.shadow").click(function(){
+		$("ul.icons").toggleClass("shadow");
+		$(this).toggleClass("active");
+		$(".language-css.shadow").toggleClass("hide");
+
+	});
+
+	$("button.gradient").click(function(){
+		$("ul.icons").toggleClass("gradient");
+		$(this).toggleClass("active");
+		$(".language-css.gradient").toggleClass("hide");
+
+	});
+
+	$("button").click(function(){
+		if ($(this).find("span").text() == "Add"){
+			$(this).find("span").text("Remove");
+		}
+		else{
+    		$(this).find("span").text("Add");
+    	};
+	});
+});
